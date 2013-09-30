1953f1ef5a0a28327ce14fdad606667b1f1e9741
iOS18 <ios18@mac11.local>
2013-09-30 20:46:42 +0700

    upload testnano.text version 4

---
+++ Demo3/Demo3Tests/Demo3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Demo3Tests.m
+//  Demo3Tests
+//
+//  Created by iOS18 on 9/30/13.
+//  Copyright (c) 2013 iOS18. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Demo3Tests : XCTestCase
+
+@end
+
+@implementation Demo3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
